9b2697441f180761ed6627d98a3ce00c0f43a628
zukaesakia <esakiazuka@gmail.com>
2022-04-19 13:16:32 +0400

    added relationships between models

--- ETickets/Models/Movie.cs
+++ ETickets/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 
@@ -20,5 +21,18 @@ namespace ETickets.Models
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Producer Producer { get; set; }
     }
 }
