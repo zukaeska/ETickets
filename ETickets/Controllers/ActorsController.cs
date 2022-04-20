376b2d62c3ffca23ddf3292d3ffc814a61293c19
zukaesakia <esakiazuka@gmail.com>
2022-04-20 14:44:07 +0400

    added actor view

--- ETickets/Controllers/ActorsController.cs
+++ ETickets/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace ETickets.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
