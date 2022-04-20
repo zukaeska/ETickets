6a260d063ad1c63091750a07fe60cd6af00cf81a
zukaesakia <esakiazuka@gmail.com>
2022-04-20 15:06:26 +0400

    add producer view

--- ETickets/Controllers/ProducersController.cs
+++ ETickets/Controllers/ProducersController.cs
@@ -17,7 +17,7 @@ namespace ETickets.Controllers
         public async Task<IActionResult> Index()
         {   
             var allProducers = await _context.Producers.ToListAsync();
-            return View();
+            return View(allProducers);
         }
     }
 }
